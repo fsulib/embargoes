156a367f1e701f272d92b7a05957b4d6e4a99455
Bryan J. Brown <bjbrown@fsu.edu>
2020-03-21 09:09:01 -0400

    Adds some custom logging functionality

--- src/Controller/EmbargoesLogController.php
+++ src/Controller/EmbargoesLogController.php
@@ -3,28 +3,55 @@
 namespace Drupal\embargoes\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
-use Drupal\examples\Utility\DescriptionTemplateTrait;
 
+/**
+ * Class EmbargoesLogController.
+ */
 class EmbargoesLogController extends ControllerBase {
 
+  public function logEmbargoEvent($data) {
+    $database = \Drupal::database();
+    $result = $database->query("INSERT INTO {embargoes_log} (time, action, node, user, embargo) VALUES ('{$data['time']}', '{$data['action']}', '{$data['node']}', '{$data['user']}','{$data['embargo']}');");
+  }
+
+  public function getRawDatabaseLog() {
+    $database = \Drupal::database();
+    $result = $database->query('SELECT * FROM {embargoes_log} ORDER BY time DESC;');
+    $raw_db_log = [];
+    foreach ($result as $record) {
+      $row = [
+        'id' => $record->id,
+        'time' => $record->time,
+        'action' => $record->action,
+        'node' => $record->node,
+        'user' => $record->user,
+        'embargo' => $record->embargo,
+      ];
+      array_push($raw_db_log, $row);
+    }
+ 
+    return $raw_db_log;
+  }
+
+  public function getFormattedDatabaseLog() {
+    $log = $this->getRawDatabaseLog();
+    $formatted_log = $log;
+
+    return $formatted_log;
+  }
+
   public function showRenderedLog() {
-
-    $rows = [
-      ['Now', 'Me', '1', 'Created', '1'],
-    ];
-
-    $table = [
+    $formatted_log = $this->getFormattedDatabaseLog();
+    $pre_rendered_log = [
       '#type' => 'table',
-      '#header' => ['Time', 'User', 'Node', 'Action', 'Embargo'],
-      '#rows' => $rows,
+      '#header' => ['ID', 'Time', 'Action', 'Node', 'User', 'Embargo'],
+      '#rows' => $formatted_log,
     ];
 
     return [
-      '#type' => '#markup',
-      '#markup' => $table,
+      '#type' => 'markup',
+      '#markup' => render($pre_rendered_log),
     ];
-
   }
 
 }
