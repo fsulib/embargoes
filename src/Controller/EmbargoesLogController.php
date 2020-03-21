e418548a4c55b0f585bf4617f00bc9b08e6f0af4
Bryan J. Brown <bjbrown@fsu.edu>
2020-03-21 11:08:11 -0400

    Formats embargo log display

--- src/Controller/EmbargoesLogController.php
+++ src/Controller/EmbargoesLogController.php
@@ -3,6 +3,7 @@
 namespace Drupal\embargoes\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\Markup;
 
 /**
  * Class EmbargoesLogController.
@@ -14,34 +15,27 @@ class EmbargoesLogController extends ControllerBase {
     $result = $database->query("INSERT INTO {embargoes_log} (time, action, node, user, embargo) VALUES ('{$data['time']}', '{$data['action']}', '{$data['node']}', '{$data['user']}','{$data['embargo']}');");
   }
 
-  public function getRawDatabaseLog() {
+  public function showRenderedLog() {
     $database = \Drupal::database();
     $result = $database->query('SELECT * FROM {embargoes_log} ORDER BY time DESC;');
-    $raw_db_log = [];
+    $formatted_log = [];
     foreach ($result as $record) {
+
+      $formatted_time = date('c', $record->time);
+      $node_title = \Drupal::entityTypeManager()->getStorage('node')->load($record->node)->get('title')->value;
+      $username = \Drupal\user\Entity\User::load($record->user)->getUsername();
+
       $row = [
         'id' => $record->id,
-        'time' => $record->time,
+        'time' => $formatted_time,
         'action' => $record->action,
-        'node' => $record->node,
-        'user' => $record->user,
+        'node' => Markup::create("<a href='/node/{$record->node}'>$node_title</a>"),
+        'user' =>  Markup::create("<a href='/user/{$record->user}'>$username</a>"),
         'embargo' => $record->embargo,
       ];
-      array_push($raw_db_log, $row);
+      array_push($formatted_log, $row);
     }
- 
-    return $raw_db_log;
-  }
 
-  public function getFormattedDatabaseLog() {
-    $log = $this->getRawDatabaseLog();
-    $formatted_log = $log;
-
-    return $formatted_log;
-  }
-
-  public function showRenderedLog() {
-    $formatted_log = $this->getFormattedDatabaseLog();
     $pre_rendered_log = [
       '#type' => 'table',
       '#header' => ['ID', 'Time', 'Action', 'Node', 'User', 'Embargo'],
