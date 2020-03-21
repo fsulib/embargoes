5818c988ce42e84d4ac64678b25a351990ba617f
Bryan J. Brown <bjbrown@fsu.edu>
2020-03-20 21:55:53 -0400

    Checkbox wrestling

--- src/Form/EmbargoesNotificationsForm.php
+++ src/Form/EmbargoesNotificationsForm.php
@@ -27,33 +27,38 @@ class EmbargoesNotificationsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('embargoes.notifications');
     $form = parent::buildForm($form, $form_state);
+
 
     $form['applications'] = array(
       '#type' => 'fieldset',
       '#title' => $this->t('Embargo Application Notifications'),
     );
-    $form['applications']['active'] = array(
+    $form['applications']['applications_active'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Enable embargo application notification emails'),
+      '#default_value' => $config->get('applications_active'),
       '#attributes' => [
         'name' => 'applications_checkbox',
       ],
     ); 
-    $form['applications']['emails'] = array(
+    $form['applications']['applications_emails'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Default email recipients'),
       '#description' => $this->t('Enter email addresses that should recieve all embargo application notification emails by default'),
+      '#default_value' => $config->get('applications_default_email_addresses'),
       '#states' => [
         'visible' => [
           ':input[name="applications_checkbox"]' => ['checked' => TRUE],
         ],
       ],
     );
-    $form['applications']['template'] = array(
+    $form['applications']['applications_template'] = array(
       '#type' => 'textarea',
       '#title' => $this->t('Application notification email template'),
       '#description' => $this->t('Modify the email template sent for embargo application notifications'),
+      '#default_value' => $config->get('applications_email_template'),
       '#states' => [
         'visible' => [
           ':input[name="applications_checkbox"]' => ['checked' => TRUE],
@@ -65,27 +70,30 @@ class EmbargoesNotificationsForm extends ConfigFormBase {
       '#type' => 'fieldset',
       '#title' => $this->t('Embargo Update Notifications'),
     );
-    $form['updates']['active'] = array(
+    $form['updates']['updates_active'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Enable embargo update notification emails'),
+      //'#default_value' => $config->get('updates_active'),
       '#attributes' => [
         'name' => 'updates_checkbox',
       ],
     ); 
-    $form['updates']['emails'] = array(
+    $form['updates']['updates_emails'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Default email recipients'),
       '#description' => $this->t('Enter email addresses that should recieve all embargo update notification emails by default'),
+      '#default_value' => $config->get('updates_default_email_addresses'),
       '#states' => [
         'visible' => [
           ':input[name="updates_checkbox"]' => ['checked' => TRUE],
         ],
       ],
     );
-    $form['updates']['template'] = array(
+    $form['updates']['updates_template'] = array(
       '#type' => 'textarea',
       '#title' => $this->t('Update notification email template'),
       '#description' => $this->t('Modify the email template sent for embargo update notifications'),
+      '#default_value' => $config->get('updates_email_template'),
       '#states' => [
         'visible' => [
           ':input[name="updates_checkbox"]' => ['checked' => TRUE],
@@ -97,17 +105,19 @@ class EmbargoesNotificationsForm extends ConfigFormBase {
       '#type' => 'fieldset',
       '#title' => $this->t('Embargo Expiration Warning Notifications'),
     );
-    $form['warnings']['active'] = array(
+    $form['warnings']['warnings_active'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Enable embargo expiration warning notification emails'),
+      //'#default_value' => $config->get('warnings_active'),
       '#attributes' => [
         'name' => 'warnings_checkbox',
       ],
     ); 
-    $form['warnings']['period'] = array(
+    $form['warnings']['warnings_period'] = array(
       '#type' => 'number',
       '#title' => $this->t('Days until expiry'),
       '#description' => $this->t('Enter the number of days before an embargo expires that an embargo expiration warning notification should be sent'),
+      '#default_value' => $config->get('warnings_expiry_period'),
       '#min' => 1,
       '#states' => [
         'visible' => [
@@ -115,20 +125,22 @@ class EmbargoesNotificationsForm extends ConfigFormBase {
         ],
       ],
     );
-    $form['warnings']['emails'] = array(
+    $form['warnings']['warnings_emails'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Default email recipients'),
       '#description' => $this->t('Enter email addresses that should recieve all embargo expiration warning notification emails by default'),
+      '#default_value' => $config->get('warnings_default_email_addresses'),
       '#states' => [
         'visible' => [
           ':input[name="warnings_checkbox"]' => ['checked' => TRUE],
         ],
       ],
     );
-    $form['warnings']['template'] = array(
+    $form['warnings']['warnings_template'] = array(
       '#type' => 'textarea',
       '#title' => $this->t('Warning notification email template'),
       '#description' => $this->t('Modify the email template sent for embargo expiration warning notifications'),
+      //'#default_value' => $config->get('warnings_email_template'),
       '#states' => [
         'visible' => [
           ':input[name="warnings_checkbox"]' => ['checked' => TRUE],
@@ -140,27 +152,30 @@ class EmbargoesNotificationsForm extends ConfigFormBase {
       '#type' => 'fieldset',
       '#title' => $this->t('Embargo Expiration Notifications'),
     );
-    $form['expirations']['active'] = array(
+    $form['expirations']['expirations_active'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Enable embargo expiration notification emails'),
+      '#default_value' => $config->get('expirations_active'),
       '#attributes' => [
         'name' => 'expirations_checkbox',
       ],
     ); 
-    $form['expirations']['emails'] = array(
+    $form['expirations']['expirations_emails'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Default email recipients'),
       '#description' => $this->t('Enter email addresses that should recieve all embargo expiration notification emails by default'),
+      '#default_value' => $config->get('expirations_default_email_addresses'),
       '#states' => [
         'visible' => [
           ':input[name="expirations_checkbox"]' => ['checked' => TRUE],
         ],
       ],
     );
-    $form['expirations']['template'] = array(
+    $form['expirations']['expirations_template'] = array(
       '#type' => 'textarea',
       '#title' => $this->t('Expiration notification email template'),
       '#description' => $this->t('Modify the email template sent for embargo expiration notifications'),
+      '#default_value' => $config->get('expirations_email_template'),
       '#states' => [
         'visible' => [
           ':input[name="expirations_checkbox"]' => ['checked' => TRUE],
@@ -176,6 +191,7 @@ class EmbargoesNotificationsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('embargoes.notifications');
+
     parent::submitForm($form, $form_state);
   }
 
