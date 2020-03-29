2a8960679c463ffd6a49a3e6e9ada2442455f946
Bryan J. Brown <bjbrown@fsu.edu>
2020-03-29 10:35:14 -0400

    Active embargo filter tests for users, now we just need IP

--- src/EmbargoesEmbargoesService.php
+++ src/EmbargoesEmbargoesService.php
@@ -40,7 +40,35 @@ class EmbargoesEmbargoesService implements EmbargoesEmbargoesServiceInterface {
   }
 
   public function getActiveEmbargoesByNode($nid, $ip, $user) {
+    $current_user_id = $user->id();
+    $active_embargoes = [];
     $embargoes = \Drupal::service('embargoes.embargoes')->getCurrentEmbargoesByNode($nid);
-    return $embargoes;
+    foreach ($embargoes as $embargo_id) {
+      $embargo = \Drupal::entityTypeManager()->getStorage('embargoes_embargo_entity')->load($embargo_id);
+
+      $exempt_users = $embargo->getExemptUsers();
+      if (!is_null($exempt_users)) {
+        $exempt_users_flattened = [];
+        foreach ($exempt_users as $exempt_user) {
+          $exempt_users_flattened[] = $exempt_user['target_id'];
+        }
+        if (in_array($current_user_id, $exempt_users_flattened)) {
+          $user_is_exempt = TRUE;
+        }
+        else {
+          $user_is_exempt = FALSE;
+        }
+      }
+
+
+      $ip_is_exempt = FALSE;
+
+
+      if ($user_is_exempt == FALSE && $ip_is_exempt == FALSE) {
+        $active_embargoes[$embargo_id] = $embargo_id;
+      }
+
+    }
+    return $active_embargoes;
   }
 }
